e0109c4fa5b8c3b1d4ff004da9be47a2b5ad142f
yusuke0626 <m1716@s.akashi.ac.jp>
2019-10-28 21:40:12 +0900

    po

--- Makefile
+++ Makefile
@@ -1,4 +1,6 @@
 Main:
 	g++ -Wall bathtowel_position.cpp -o Bath -std=c++11 -pthread -lrt
 Limit:
-	g++ -Wall limit_test.cpp -o Limit -lpigpio -std=c++11 -pthread
+	g++ -Wall limit_test.cpp -o Limit PigpioMS.o -lpigpio -std=c++17 -pthread -lrt
+PigpioMS.o: ./PigpioMS/PigpioMS.cpp
+	g++ -Wall -c ./PigpioMS/PigpioMS.cpp -lpigpio -std=c++17 -pthread 
--- limit_test.cpp
+++ limit_test.cpp
@@ -1,17 +1,25 @@
 #include<iostream>
 #include<pigpio.h>
 #include<stdlib.h>
+#include"PigpioMS/PigpioMS.hpp"
+
+using namespace std;
+using namespace RPMS;
+
+MotorSerial ms;
 
 int main(){
-	std::cout << "リミットスイッチチェッカー起動しました" << std::endl;
-	std::cout << "Ctrl + C でプログラム終了します" << std::endl;
-	std::cout << "通電中には０が、スイッチが押されているときは１が表示され続けます" << std::endl;
-	std::cout << "スイッチから手を離してください" << std::endl;
-	std::cout << "確認したいピンの番号を入力してください" << std::endl;
-	std::cout << "M-2 右上　→　12 右下　→　16 左上　→  11 左下　→　22" << std::endl;
-	std::cout << "M-3 Y軸前　→　26　Y軸後ろ　→　19　Z軸上　→　9　Z軸下　→　10　" << std::endl;
+	cout << "リミットスイッチチェッカー起動しました                          " << endl;
+	cout << "Ctrl + C でプログラム終了します                                 " << endl;
+	cout << "通電中には０が、スイッチが押されているときは１が表示され続けます" << endl;
+	cout << "スイッチから手を離してください                                  " << endl;
+	cout << "確認したいピンの番号を入力してください                          " << endl;
+	cout << "M-2 右上　→　12 右下　→　16 左上　→  11 左下　→　22             " << endl;
+	cout << "M-3 Y軸前　→　26　Y軸後ろ　→　19　Z軸上　→　9　Z軸下　→　10　   " << endl;
+	cout << "ポテンショメータチェッカーは、100を入力してください             " << endl;
 	int pin;
-	std::cin >> pin;
+	cin >> pin;
+
 
 	gpioInitialise();
 	gpioSetMode(pin,PI_INPUT);
@@ -19,15 +27,23 @@ int main(){
 
 	int x;
 	x = gpioRead(pin);
-	
-	if( x == 1){
-		std::cout << "通電していません。配線及びスイッチの確認をしてください。" << std::endl;
-		exit(0);
+	if(pin != 100){
+		if( x == 1){
+			cout << "通電していません。配線及びスイッチの確認をしてください。" << endl;
+			exit(0);
+		}else{
+			while(1){
+				x = gpioRead(pin);
+				cout << x << endl;
+			}
+		}
 	}else{
+		int x;
+		x = ms.send(16,40,200);
 		while(1){
-			x = gpioRead(pin);
-			std::cout << x << std::endl;
+			cout <<"potentiometer" <<  x << endl;
 		}
 	}
 	return 0;
+
 }
